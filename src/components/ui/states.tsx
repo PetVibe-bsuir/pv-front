06f550e913065d827b8731fdce758f8235a3a697
maks2 <maks210306@yandex.by>
2024-10-18 17:52:12 +0300

    states devolpment

--- src/components/ui/states.tsx
+++ src/components/ui/states.tsx
@@ -3,8 +3,8 @@ import StatesCard from "@/components/ui/statesCard.tsx";
 const States = () => {
     return (
         <div className="flex flex-col justify-between items-center gap-3.5">
-            <StatesCard indicator={0} title="здоровье" color="bg-cyan-500" />
-            <StatesCard indicator={0} title="комфорт" color="bg-purple-500" />
+            <StatesCard indicator={32} title="здоровье" color="bg-cyan-500" />
+            <StatesCard indicator={100} title="комфорт" color="bg-purple-500" />
             <StatesCard indicator={0} title="энергия" color="bg-yellow-500" />
         </div>
     );
--- src/components/ui/statesCard.tsx
+++ src/components/ui/statesCard.tsx
@@ -5,11 +5,23 @@ interface StatestProps {
 }
 
 const StatesCard = ({ indicator, title, color }: StatestProps) => {
+    // Ограничим индикатор в диапазоне от 0 до 100
+    const clampedIndicator = Math.max(0, Math.min(indicator, 100));
+
     return (
-        <div className="flex items-center rounded-lg p-4 border border-gray-300 w-full">
+        <div className="flex items-center rounded-lg p-4 w-full">
             <div className={`w-4 h-4 rounded-full ${color} mr-2`}></div>
-            <div className="flex-1">
-                <span>{indicator}/100</span>
+            <div className="flex-1 relative h-8 rounded-md overflow-hidden bg-gray-300">
+                <div
+                    className={`absolute left-0 top-0 h-full ${color}`}
+                    style={{ width: `${clampedIndicator}%` }}
+                ></div>
+                <span
+                    className="text-black font-bold text-center absolute inset-0 flex items-center justify-center"
+                    style={{ zIndex: 1 }}
+                >
+                    {indicator}/100
+                </span>
             </div>
             <div className="flex-1 text-center">
                 <span>{title}</span>
