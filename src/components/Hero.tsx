0041cc30f462d10e6719c1f3566b15972df81365
shulmnn <matvejsh17@gmail.com>
2024-10-18 19:00:14 +0300

    add hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+// type HeroProps {
+
+// }
+
+// const Hero:FC<HeroProps> = () => {
+const Hero = () => {
+    return (
+        <div className="flex flex-1 items-center justify-between">
+            <img
+                className="h-[50vw] w-[50vw]"
+                src="https://utfs.io/f/bkysktwhKky7zZkMcMoyOVoQAJU8haN1zf03sc2ukPFIK9xL"
+            ></img>
+        </div>
+    );
+};
+
+export default Hero;
