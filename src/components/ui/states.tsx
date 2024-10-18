2819fed17e0dbdc61e91d3c8b2908c70a98fce9c
maks2 <maks210306@yandex.by>
2024-10-18 18:41:13 +0300

    states icons

--- src/components/ui/states.tsx
+++ src/components/ui/states.tsx
@@ -10,7 +10,7 @@ const States = () => {
 
     return (
         <div className="flex flex-col justify-between items-center gap-3.5">
-            <StatesCard indicator={32} color="bg-cyan-500" lucideImg={heartPulseSvg} />
+            <StatesCard indicator={90} color="bg-cyan-500" lucideImg={heartPulseSvg} />
             <StatesCard indicator={100} color="bg-purple-500" lucideImg={comfortSvg} />
             <StatesCard indicator={90} color="bg-yellow-500" lucideImg={activeSvg} />
         </div>
