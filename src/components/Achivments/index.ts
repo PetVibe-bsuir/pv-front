2147dfc6e5835b49a93809a0c0f0f80dbe9f4fad
maks2 <maks210306@yandex.by>
2024-10-19 16:58:08 +0300

    fix bugs

--- src/components/Achivments/index.ts
+++ src/components/Achivments/index.ts
@@ -1,1 +1,3 @@
 export { default as AchivmentPage } from "./Achivments.tsx";
+export { default as AchievementsList } from "./AchievementsList.tsx";
+export { default as AchivmentsCard } from "./AchivmentsCard.tsx";
